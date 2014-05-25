70c499d074725b70a22d6a80730b0559a40d95ab
Shane Smith <shane.smith.baz@gmail.com>
2014-05-25 14:54:27 -0400

    Added agignore

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOTFILES="bashrc bash_aliases bashrc-sshlogin gitconfig gitignore vimrc vim ackrc inputrc colordiffrc fzf npmrc"
+DOTFILES="bashrc bash_aliases bashrc-sshlogin gitconfig gitignore vimrc vim ackrc inputrc colordiffrc fzf npmrc agignore"
 
 is_linux=
 is_mac=
