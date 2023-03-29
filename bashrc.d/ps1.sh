a5c756db4cbd2d40caf84cb4053823bddfefe456
Shane Smith <shane.wm.smith@gmail.com>
2023-03-29 17:44:10 -0400

    bash: fix ps1 shellcheck errors, remove unused statuses

--- bashrc.d/ps1.sh
+++ bashrc.d/ps1.sh
@@ -6,14 +6,19 @@ export GIT_PS1_SHOWUNTRACKEDFILES=true
 export GIT_PS1_SHOWUPSTREAM="name"
 export GIT_PS1_DESCRIBE_STYLE=branch
 
-save_function() {
-    local ORIG_FUNC=$(declare -f $1)
-    local NEWNAME_FUNC="$2${ORIG_FUNC#$1}"
-    eval "$NEWNAME_FUNC"
-}
-
 if ! type -t __orig_git_ps1_show_upstream >/dev/null; then
-  save_function __git_ps1_show_upstream __orig_git_ps1_show_upstream
+  __save_function() {
+    local ORIG_FUNC NEWNAME_FUNC
+
+    ORIG_FUNC=$(declare -f "$1")
+    NEWNAME_FUNC="$2${ORIG_FUNC#"$1"}"
+
+    eval "$NEWNAME_FUNC"
+  }
+
+  __save_function __git_ps1_show_upstream __orig_git_ps1_show_upstream
+
+  unset -f __save_function
 fi
 
 __colorize_ps1() {
@@ -23,72 +28,21 @@ __colorize_ps1() {
 __git_ps1_show_upstream() { 
   __orig_git_ps1_show_upstream
 
-  local branch="$(git rev-parse --symbolic-full-name --abbrev-ref @)"
-  local upstream="$(git rev-parse --symbolic-full-name --abbrev-ref '@{upstream}' 2>/dev/null)"
+  local branch upstream remote remote_branch
 
-  local remote="$(sed 's#/.*##' <<<"$upstream")"
-  local remote_branch="$(sed 's#[^/]*/##' <<<"$upstream")"
+  branch="$(git rev-parse --symbolic-full-name --abbrev-ref @)"
+  upstream="$(git rev-parse --symbolic-full-name --abbrev-ref '@{upstream}' 2>/dev/null)"
+  remote="${upstream%%/*}"
+  remote_branch="${upstream#*/}"
 
   if [[ -n $upstream ]]; then
-    if [[ $branch == $remote_branch ]]; then
+    if [[ $branch == "$remote_branch" ]]; then
       p="$p${remote}"
     else
       p="$p${upstream}"
     fi
     b="$b $p"
     p=""
-  fi
-}
-
-__vagrant_status() {
-
-  local vagrantdir="$PWD"
-
-  while [[ ! -d "$vagrantdir/.vagrant" && $vagrantdir != "/" ]]; do
-    vagrantdir=$(dirname "$vagrantdir")
-  done
-
-  vagrantdir="$vagrantdir/.vagrant"
-
-  if [[ ! -d $vagrantdir ]]; then
-    return
-  fi
-
-  local -a RunningVMs=()
-  local -a RunningUUIDs=()
-  local -a VMs=()
-  local -a DeadVMs=()
-
-  if [[ $(uname -s) == "Darwin" ]]; then
-    local proclist="$(ps -ww -o args= -p $(pgrep VBoxHeadless) 2>/dev/null)"
-  else
-    local proclist="$(ps h -C VBoxHeadless -o args)"
-  fi
-
-  if [[ -z $proclist ]]; then
-    return
-  fi
-
-  # Gather the UUIDs of all presently running VMs.
-  while read proc; do
-    proc=${proc#*--startvm[[:space:]]}
-    proc=${proc%%[[:space:]]*}
-    RunningUUIDs+=(${proc})
-  done <<< "$proclist"
-
-  for machinedir in $vagrantdir/machines/*; do
-    if [[ -r "$machinedir/virtualbox/id" ]]; then
-      local machinename=$(basename $machinedir)
-      if [[ "${RunningUUIDs[@]}" =~ $(cat "$machinedir/virtualbox/id") ]]; then
-        VMs+=($machinename)
-      fi
-    fi
-  done
-
-  local output="${VMs[*]}"
-
-  if [[ -n $output ]]; then
-    echo " <$(echo $output | tr ' ' ',')>"
   fi
 }
 
@@ -100,16 +54,6 @@ __ssh_keys_status() {
   echo -e " {\xC2\xA7}"
 }
 
-__docker_compose_status() {
-  if ! command -v docker-compose >/dev/null || [[ -z "$COMPOSE_FILE" ]]; then
-    return
-  fi
-
-  local status=$(echo "$COMPOSE_FILE" | sed -e 's/docker-compose\.yml/./' -e 's/docker-compose\.//' -e 's/\.yml//')
-
-  echo " +${status}+"
-}
-
 if ! type __git_ps1 >/dev/null; then
   __git_ps1() {
     return
@@ -117,7 +61,8 @@ if ! type __git_ps1 >/dev/null; then
 fi
 
 __jobs_status() {
-  local cnt=$(jobs | awk '/^\[[0-9]+\]/ && $2 !~ /^Done/' | wc -l | awk '{print $1}')
+  local cnt
+  cnt=$(jobs | awk '/^\[[0-9]+\]/ && $2 !~ /^Done/' | wc -l | awk '{print $1}')
 
   if [[ $cnt -eq 0 ]]; then
     return
@@ -163,7 +108,7 @@ TIME="$(__colorize_ps1 LK '\A')"
 
 HOST=$([[ -n $SPIN ]] && sed "s/\\..*//" < /etc/spin/machine/fqdn || echo '\h')
 USER_HOST_COLOR=$([ -n "$SSH_CLIENT" ] && echo "Y" || echo "G" )
-USER_HOST="$(__colorize_ps1 $USER_HOST_COLOR "\\u@$HOST")"
+USER_HOST="$(__colorize_ps1 "$USER_HOST_COLOR" "\\u@$HOST")"
 
 CWD="$(__colorize_ps1 C '\w')"
 
@@ -176,8 +121,6 @@ ${USER_HOST}\
 ${CWD}\
 \$(__ssh_keys_status)\
 \$(__jobs_status)\
-\$(__vagrant_status)\
-\$(__docker_compose_status)\
 \$(__git_ps1 ' (%s)')\
 \$(__rosetta)\
 \$(__dev_dev)\
