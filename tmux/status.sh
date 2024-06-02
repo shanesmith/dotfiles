96d4983411de462e8d7be5e013884c57e9b2eac8
Shane Smith <shane.wm.smith@gmail.com>
2024-06-01 23:31:29 -0400

    tmux-status: better docker disk usage check, add timeouts, color values

--- tmux/status.sh
+++ tmux/status.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 WHITE="#FFFFFF"
+LIGHT_GRAY="#AAAAAA"
+DARK_GRAY="#444444"
 BLACK="#202020"
 RED="#EF8E7D"
 YELLOW="#E8E800"
@@ -21,13 +23,26 @@ _trim() {
   echo -n "$var"
 }
 
+_color() {
+  local fg="fg=$1"
+  local bg="${2:+,bg=$2}"
+  echo -n "#[${fg}${bg}]"
+}
+
+_deco() {
+  local decoration="$1"
+  local text="$2"
+  local no_decoration=$(echo -n "${decoration}" | sed -E 's/(^|,)/\1no/g')
+  echo -n "#[${decoration}]${text}#[${no_decoration}]"
+}
+
 _status() {
   local color=$1
   local inverse=$2
   local text=$3
-  local bg="#444444"
-  local arrow="#444444"
-  local arrow_bg="#202020"
+  local bg="${DARK_GRAY}"
+  local arrow="${DARK_GRAY}"
+  local arrow_bg="${BLACK}"
 
   if [[ $inverse == "1" ]]; then
     local tmp=$bg
@@ -36,11 +51,25 @@ _status() {
     color=$tmp
   fi
 
-  echo -n "#[fg=$arrow,bg=$arrow_bg,nobold,nounderscore,noitalics]#[fg=$color,bg=$bg,nobold,nounderscore,noitalics] $text #[fg=$arrow,bg=$arrow_bg,nobold,nounderscore,noitalics]"
+  echo -n "$(_color $arrow $arrow_bg)$(_color $color $bg) $text $(_color $arrow $arrow_bg)"
+}
+
+_attention() {
+  local value="$1"
+  local warn_level="$2"
+  local danger_level="$3"
+
+  if [[ ${value%%%} -gt ${danger_level} ]]; then
+    value=$(_color $RED)${value}$(_color $WHITE)
+  elif [[ ${value%%%} -gt ${warn_level} ]]; then
+    value=$(_color $YELLOW)${value}$(_color $WHITE)
+  fi
+
+  echo -n "${value}"
 }
 
 docker-desktop-shell() {
-  bkt -- docker run --rm --privileged --pid=host alpine nsenter -t 1 -m -u -n -i -- "$@"
+  bkt -- timeout 2 docker run --rm --privileged --pid=host alpine nsenter -t 1 -m -u -n -i -- "$@"
 }
 
 docker_count() {
@@ -48,17 +77,23 @@ docker_count() {
     return
   fi
 
-  if ! docker info >/dev/null 2>&1; then
+  if ! timeout 2 docker info >/dev/null 2>&1; then
     _status $RED 1 "🐳"
     return
   fi
 
-  local count=$(bkt --ttl=5s -- docker ps -q | wc -l | _trim)
-  local disk_usage=$(BKT_TTL=60s docker-desktop-shell df -h /dev/vda1 | awk 'NR == 2 { print $5 }')
+  local rawfile="$HOME/Library/Containers/com.docker.docker/Data/vms/0/data/Docker.raw"
+
+  local count=$(BKT_TTL=5s bkt -- timeout 2 docker ps -q | wc -l | _trim)
+  local disk_usage=$(stat -f '%b %z' "${rawfile}" | awk '{ printf("%d%%", ($1*512/$2)*100) }')
   local cpu_usage=$(BKT_TTL=10s docker-desktop-shell top -b -n 1 -d 5 | awk '/^CPU/ { print 100-$8 "%" }')
   local mem_usage=$(BKT_TTL=30s docker-desktop-shell free | awk 'NR == 2 { printf("%d%%", ($3/$2)*100) }')
 
-  _status $WHITE 0 "🐳 ${count} | ${cpu_usage} | ${mem_usage} | ${disk_usage}"
+  disk_usage=$(_attention "$disk_usage" 70 90)
+  cpu_usage=$(_attention "$cpu_usage" 70 90)
+  mem_usage=$(_attention "$mem_usage" 70 90)
+
+  _status $WHITE 0 "🐳 ${count}  $(_deco dim "C:")${cpu_usage} $(_deco dim "M:")${mem_usage} $(_deco dim "D:")${disk_usage}"
 }
 
 kubectl_context() {
@@ -67,11 +102,9 @@ kubectl_context() {
   fi
 
   local title context namespace
-  title="󱃾"
-  # title="#[fg=#AAAAAA,nobold,nounderscore,noitalics]k8s#[fg=$WHITE,nobold,nounderscore,noitalics]"
+  title="$(_color blue)󱃾$(_color $WHITE)"
   context=$(kubectl config current-context)
   namespace=$(kubectl config get-contexts --no-headers "${context}" | awk '{print $5}')
-
 
   _status $WHITE 0 "${title} ${context}  ${namespace:-default}"
 }
