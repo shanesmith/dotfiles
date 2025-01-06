4d4645f380e98005fe7616887d9b834219890c04
Shane Smith <shane.wm.smith@gmail.com>
2025-01-06 11:57:28 -0500

    mac-prefs: set dock autohide, hot corners, trackpad speed and disable double space dot

--- mac-prefs.sh
+++ mac-prefs.sh
@@ -16,16 +16,23 @@ defaults write com.apple.dock show-recents -bool false
 
 defaults write com.apple.dock expose-group-apps -bool true
 
+defaults write com.apple.dock autohide -bool true
+
 defaults write com.apple.WindowManager StandardHideDesktopIcons -bool true
 
 defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool false
 
 defaults write com.apple.dock wvous-tr-corner -int 5
+defaults write com.apple.dock wvous-br-corner -int 1
+defaults write com.apple.dock wvous-bl-corner -int 1
+defaults write com.apple.dock wvous-bl-corner -int 1
 
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehaviour -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehaviour -int 1
+
+defaults write com.apple.trackpad.scaling -int 1
 
 # Disable Notifaction Centre Swipe left from right edge with two fingers
 defaults write com.apple.AppleMultitouchTrackpad TrackpadTwoFingerFromRightEdgeSwipeGesture -int 0
@@ -68,6 +75,8 @@ defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 defaults write com.apple.finder ShowRecentTags -bool false
 
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
+
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
 
 killall Dock
 killall Finder
